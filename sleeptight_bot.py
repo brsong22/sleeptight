8d97319073e9f641390d4611c62a1f8510b10d72
Richard Song <brsong22@gmail.com>
2018-05-31 10:18:27 -0500

    add read/write submission id tracking
    read/write ids to files to record and keep track of visited submissions
    so we can update only new stories that we have not read yet.
    
    limit search to top 3 stories
    
    update string formating to use .format() method

--- sleeptight_bot.py
+++ sleeptight_bot.py
@@ -1,17 +1,28 @@
 import praw
 
-r = praw.Reddit('sleeptight')
 
-subreddit = r.subreddit('nosleep')
-
-top_visited = []
+try:
+	f_visited = open("visited_posts.txt", "r+")
+	old_visited = f_visited.read().split(",")
+	if len(old_visited) > 1:
+		append_string = ","
+	else:
+		append_string = ""
+except IOError:
+	f_visited = open("visited_posts.txt", "w+")
+	old_visited = []
+	append_string = ""
 new_visited = []
 
+r = praw.Reddit('sleeptight')
+subreddit = r.subreddit('nosleep')
 
-for post in subreddit.top(time_filter='day'):
+for post in subreddit.top(time_filter='day', limit=3):
 	if len(new_visited) < 3:
-		if not post.id in top_visited:
-			top_visited.append(post.id)
+		if not post.id in old_visited:
+			record = "{0}{1}".format(append_string, post.id)
+			append_string = ","
+			f_visited.write(record)
 			submission = {'id': post.id,
 						  'title': post.title,
 						  'author': post.author,
@@ -19,6 +30,13 @@ for post in subreddit.top(time_filter='day'):
 			new_visited.append(submission)
 	else:
 		break
-print("These are the top 3 nosleep stories trending in the last day:")
-for i, s in enumerate(new_visited):
-	print(i+1,".) ", s['title']," by: ",s['author']," (",s['upvotes']," upvotes)")
+if len(new_visited) == 1:
+	print("There is {0} new /r/nosleep story trending in the last 24 hours:".format(len(new_visited)))
+	for i, s in enumerate(new_visited):
+		print("{0}.) {1} by: {2} ({3} upvotes)".format(i+1, s['title'], s['author'], s['upvotes']))
+elif len(new_visited) > 1:
+	print("There are {0} new /r/nosleep stories trending in the last 24 hours:".format(len(new_visited)))
+	for i, s in enumerate(new_visited):
+		print("{0}.) {1} by: {2} ({3} upvotes)".format(i+1, s['title'], s['author'], s['upvotes']))
+else:
+	print("There are no new top 3 stories within the last 24 hours.")
