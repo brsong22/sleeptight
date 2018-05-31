a17950c7ae1ba53de8728e8ec447b97e6dc168fe
Richard Song <brsong22@gmail.com>
2018-05-31 11:32:29 -0500

    add reddit message notification
    added reddit message functionality to send private message to user with
    list and link to top 3 nosleep stories in last 24 hours

--- sleeptight_bot.py
+++ sleeptight_bot.py
@@ -26,17 +26,32 @@ for post in subreddit.top(time_filter='day', limit=3):
 			submission = {'id': post.id,
 						  'title': post.title,
 						  'author': post.author,
-						  'upvotes': post.ups}
+						  'upvotes': post.ups,
+						  'permalink': post.permalink}
 			new_visited.append(submission)
 	else:
 		break
+
+msg_subject = ""
+msg_body = ""
 if len(new_visited) == 1:
+	msg_subject = "There is {0} new /r/nosleep story trending in the last 24 hours:".format(len(new_visited))
 	print("There is {0} new /r/nosleep story trending in the last 24 hours:".format(len(new_visited)))
 	for i, s in enumerate(new_visited):
+		msg_body += "{0}.) {1} by: {2} ({3} upvotes)\n\n".format(i+1, s['title'], s['author'], s['upvotes'])
+		msg_body += "{0}\n\n".format(s['permalink'])
 		print("{0}.) {1} by: {2} ({3} upvotes)".format(i+1, s['title'], s['author'], s['upvotes']))
 elif len(new_visited) > 1:
+	msg_subject = "There are {0} new /r/nosleep stories trending in the last 24 hours:".format(len(new_visited))
 	print("There are {0} new /r/nosleep stories trending in the last 24 hours:".format(len(new_visited)))
 	for i, s in enumerate(new_visited):
+		msg_body += "{0}.) {1} by: {2} ({3} upvotes)\n\n".format(i+1, s['title'], s['author'], s['upvotes'])
+		msg_body += "{0}\n\n".format(s['permalink'])
 		print("{0}.) {1} by: {2} ({3} upvotes)".format(i+1, s['title'], s['author'], s['upvotes']))
+		print(s['permalink'])
 else:
+	msg_subject = "There are no new top 3 stories within the last 24 hours."
+	msg_body += "There are no new top stories to read since the last time you checked.\n\nSleep easy!"
 	print("There are no new top 3 stories within the last 24 hours.")
+
+r.redditor('songbirdy').message(msg_subject, msg_body)
